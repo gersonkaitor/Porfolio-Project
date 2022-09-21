13dd09f6732beccc068c1ebec69df82ca13d8e85
gersonkaitor <gersonkaitor@gmail.com>
2022-09-21 11:09:43 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {Routes, Route} from 'react-router-dom';
+import Home from './containers/home'
+import About from './containers/about'
+import Contact from './containers/contact'
+import Skills from './containers/skills'
+import Portfolio from './containers/portfolio'
+import Resume from './containers/resume'
+import Navbar from "./components/navBar"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+    {/* particles js*/}
+    {/* navbar component */}
+    <Navbar />
+    {/* main page content */}
+    <Routes>
+      {/* create all routes*/}
+      <Route path="/" index element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/skills" element={<Skills/>}/>
+      <Route path="/resume" element={<Resume/>}/>
+      <Route path="/portfolio" element={<Portfolio/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes>
+  </div>;
 }
 
 export default App;
---
+++ src/components/navBar/config.js
@@ -0,0 +1,28 @@
+const navMenus = [
+    {
+        label : "HOME",
+        to : '/'
+    },
+    {
+        label : "ABOUT ME",
+        to : '/about'
+    },
+    {
+        label : "SKILLS",
+        to : '/skills'
+    },
+    {
+        label : "RESUME",
+        to : '/resume'
+    },
+    {
+        label : "PORTFOLIO",
+        to : '/portfolio'
+    },
+    {
+        label : "CONTACT",
+        to : '/contact'
+    },
+]
+
+export default navMenus;
