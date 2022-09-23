6a1fcf0bd8b60ecf3ea38fa05da8d0e23294ef2e
gersonkaitor <gersonkaitor@gmail.com>
2022-09-23 11:12:02 +0800

    Add Particle Js and section to the  Home Page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
-import "./App.css";
-import {Routes, Route} from 'react-router-dom';
-import Home from './containers/home'
-import About from './containers/about'
-import Contact from './containers/contact'
-import Skills from './containers/skills'
-import Portfolio from './containers/portfolio'
-import Resume from './containers/resume'
-import Navbar from "./components/navBar"
+import "./App.scss";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import Home from "./containers/home";
+import About from "./containers/about";
+import Contact from "./containers/contact";
+import Skills from "./containers/skills";
+import Portfolio from "./containers/portfolio";
+import Resume from "./containers/resume";
+import Navbar from "./components/navBar";
+import particlesConfig from "./helpers/particlesConfig";
 
 function App() {
-  return <div className="App">
-    {/* particles js*/}
-    {/* navbar component */}
-    <Navbar />
-    {/* main page content */}
-    <Routes>
-      {/* create all routes*/}
-      <Route path="/" index element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/skills" element={<Skills/>}/>
-      <Route path="/resume" element={<Resume/>}/>
-      <Route path="/portfolio" element={<Portfolio/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-    </Routes>
-  </div>;
+  const particlesInit = async (main) => {
+    await loadFull(main);
+  };
+
+  const location = useLocation();
+  const renderParticleJsHomePage = location.pathname === "/";
+
+  return (
+    <div className="App">
+      {/* particles js*/}
+      {renderParticleJsHomePage && (
+        <Particles
+          id="particles"
+          options={particlesConfig}
+          init={particlesInit}
+        />
+      )}
+
+      {/* navbar component */}
+      <div className="App-navbar-wrapper">
+        <Navbar />
+      </div>
+
+      {/* main page content */}
+      <div className="App-main-content-wrapper">
+        <Routes>
+          {/* create all routes*/}
+          <Route path="/" index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      
+    </div>
+  );
 }
 
 export default App;
--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import {Animate} from 'react-simple-animate'
+import {FaFacebook, FaLinkedin, FaGithub } from 'react-icons/fa';
+import './styles.scss';
+
+
 
 const Home = () =>{
     return(
-        <div>
-            Home
-        </div>
+        <section className='home' id="home">
+            <div className='home-text-wrapper'>
+                <h1>
+                    Hello, I'm Gerson <br/>
+                    Web Developer
+                </h1>
+            </div>
+            <Animate
+                play={true}
+                duration={1.5}
+                delay={1}
+                start={{
+                    transform : 'translateY(600px)'
+                }}
+                end={{
+                    transform : 'translateX(0)'
+                }}
+            >
+                <div className='contact-me'>
+                    <div className='contact-me-buttons-wrapper'>
+                        <button>Hire me</button>
+                        <a>Download Resume</a>
+                    </div>
+                    <div className='contact-me-social-wrapper'>
+                        <FaLinkedin size={40}/>
+                        <FaGithub size={40}/>
+                        <FaFacebook size={40}/>
+                    </div>
+                </div>
+            </Animate>
+
+        </section>
     );
 }
 
